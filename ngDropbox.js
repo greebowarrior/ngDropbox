bf7d9328f2bbe907f08197ea19a6215bce8c9f1e
Martin Purcell <martin.purcell@gmail.com>
2014-10-28 17:28:19 +0000

    Tidying

--- ngDropbox.js
+++ ngDropbox.js
@@ -48,6 +48,7 @@ angular.module('ngDropbox', ['ngResource'])
 			
 			return {
 				self: this,
+				settings: this.settings,
 				
 				oauth1: function(){
 					// Shorthand method for OAuth 1.0
@@ -75,10 +76,11 @@ angular.module('ngDropbox', ['ngResource'])
 				access_token: function(){
 					var deferred = $q.defer();
 					$http.post('https://api.dropbox.com/1/oauth/access_token', null, {
-						headers: {
+						'headers': {
 							'Authorization': 'OAuth oauth_version="1.0", oauth_signature_method="PLAINTEXT", oauth_consumer_key="'+settings.app_key+'", oauth_token="'+settings.request.token+'", oauth_signature="'+settings.app_secret+'&'+settings.request.secret+'"'
 						}
-					}).success(function(response){
+					})
+					.success(function(response){
 						// Parse response
 						try {
 							settings.oauth.secret = response.match(/oauth_token_secret=([\w]+)/)[1];
@@ -87,7 +89,8 @@ angular.module('ngDropbox', ['ngResource'])
 						} catch(e){
 							$log.error(e.message)
 						}
-					}).error(function(error){
+					})
+					.error(function(error){
 						deferred.reject(error);
 					})
 					return deferred.promise;
@@ -118,10 +121,11 @@ angular.module('ngDropbox', ['ngResource'])
 				request_token: function(){
 					var deferred = $q.defer();
 					$http.post('https://api.dropbox.com/1/oauth/request_token', null, {
-						headers: {
+						'headers': {
 							'Authorization': 'OAuth oauth_version="1.0", oauth_signature_method="PLAINTEXT", oauth_consumer_key="'+settings.app_key+'", oauth_signature="'+settings.app_secret+'&"'
 						}
-					}).success(function(response){
+					})
+					.success(function(response){
 						try {
 							settings.request.token	= response.match(/oauth_token=([\w]+)/i)[1];
 							settings.request.secret	= response.match(/oauth_token_secret=([\w]+)/)[1];
@@ -130,38 +134,38 @@ angular.module('ngDropbox', ['ngResource'])
 							$log.error(e.message)
 							return deferred.reject();
 						}
-					}).error(function(response){
+					})
+					.error(function(response){
 						deferred.reject(response.error);
 					});
 					return deferred.promise;
 				},
 				
-				/*
 				token_from_oauth1: function(){
-					
-					$http.post('https://api.dropbox.com/1/oauth2/token_form_oauth1', null, {
-						headers: {
-							'Authorization': 'OAuth oauth_version="1.0", oauth_signature_method="PLAINTEXT", oauth_consumer_key="'+settings.app_key+'", oauth_token="'+settings.request.token+'", oauth_signature="'+settings.app_secret+'&'+settings.request.secret+'"'
-						}
-					}).success(function(response){
-						// Parse response
-					}).error(function(error){
-						
-					})
-
-					$http.post('https://api.dropbox.com/1/oauth2/token_from_oauth1').success(function(json){
-				//		if (json.access_token) self.setToken(json.access_token);
-						return json.access_token;
-					});
+					var deferred = $q.defer();
+					if (!settings.oauth2 && settings.oauth.token){
+						$http.post('https://api.dropbox.com/1/oauth2/token_from_oauth1', null, {
+							'headers': authorizationHeader()
+						})
+						.success(function(response){
+							if (response.access_token){
+								settings.oauth2 = response.access_token;
+								return deferred.resolve(response);
+							}
+							return deferred.reject(response);
+						})
+						.error(function(error){
+							deferred.reject(error);
+						});
+					}
+					return deferred.promise;
 				},
-				*/
-				
 				
 				/** OAuth 2 **/
 				authorize_v2: function(){
 					// OAuth2...
 					var self = this, deferred = $q.defer();
-					var authUrl = 'https://www.dropbox.com/1/oauth2/authorize?client_id='+ this.clientId +'&response_type=token&redirect_uri=' + this.redirectUri
+					var authUrl = 'https://www.dropbox.com/1/oauth2/authorize?client_id='+ settings.app_key +'&response_type=token&redirect_uri=' + settings.redirectUri
 					function listener(event) {
 						if (event.data){
 							var response = {}, data = event.data.replace(/^#/, '').split('&');
@@ -184,66 +188,66 @@ angular.module('ngDropbox', ['ngResource'])
 				},
 				
 				account: function(){
-					return $http.get('https://api.dropbox.com/1/account/info', {headers: authorizationHeader()});
+					return $http.get('https://api.dropbox.com/1/account/info', {'headers': authorizationHeader()});
 				},
 				datastore: {
 					// TODO: datastore methods
 					create: function(id){
-						return $http.post('https://api.dropbox.com/1/datastores/get_or_create_datastore', null, {params:{dsid:id}});
+						return $http.post('https://api.dropbox.com/1/datastores/get_or_create_datastore', null, {'params':{'dsid':id}});
 					},
 					delete: function(id){
-						return $http.post('https://api.dropbox.com/1/datastores/delete_datastore', null, {params:{handle:id}});
+						return $http.post('https://api.dropbox.com/1/datastores/delete_datastore', null, {'params':{'handle':id}});
 					},
 					get: function(datastore){
-						return $http.get('https://api.dropbox.com/1/datastores/get_datastore', {params:{dsid:id}, headers: authorizationHeader()});
+						return $http.get('https://api.dropbox.com/1/datastores/get_datastore', {'params':{'dsid':id}, 'headers': authorizationHeader()});
 					},
 					list: function(){
-						return $http.get('https://api.dropbox.com/1/datastores/list_datastores', {headers: authorizationHeader()});
+						return $http.get('https://api.dropbox.com/1/datastores/list_datastores', {'headers': authorizationHeader()});
 					}
 				},
 				fileops: {
 					copy: function(from,to){
-						return $http.post('https://api.dropbox.com/1/fileops/copy', null, {params:{root: 'auto', from_path: from, to_path: to}, headers: authorizationHeader()});
+						return $http.post('https://api.dropbox.com/1/fileops/copy', null, {'params':{'root': 'auto', 'from_path': from, 'to_path': to}, 'headers': authorizationHeader()});
 					},
 					create_folder: function(path){
-						return $http.post('https://api.dropbox.com/1/fileops/create_folder', null, {params:{root:'auto',path:path}, headers: authorizationHeader()});
+						return $http.post('https://api.dropbox.com/1/fileops/create_folder', null, {'params':{'root':'auto','path':path}, 'headers': authorizationHeader()});
 					},
 					delete: function(path){
-						return $http.post('https://api.dropbox.com/1/fileops/delete', null, {params:{root:'auto',path:path}, headers: authorizationHeader()});
+						return $http.post('https://api.dropbox.com/1/fileops/delete', null, {'params':{'root':'auto','path':path}, 'headers': authorizationHeader()});
 					},
 					move: function(from,to){
-						return $http.post('https://api.dropbox.com/1/fileops/move', null, {params:{root: 'auto', from_path: from, to_path: to}, headers: authorizationHeader()});
+						return $http.post('https://api.dropbox.com/1/fileops/move', null, {'params':{'root': 'auto', 'from_path': from, 'to_path': to}, 'headers': authorizationHeader()});
 					}
 				},
 				files: function(path){
 					var params = {};
 					if (typeof(rev) != 'undefined') params.rev = rev;
-					return $http.get('https://api-content.dropbox.com/1/files/auto/'+path, {params: params, headers: authorizationHeader()});
+					return $http.get('https://api-content.dropbox.com/1/files/auto/'+path, {'params': params, 'headers': authorizationHeader()});
 				},
 				files_put: function(path, data){
 					// Upload file to dropbox
-					return $http.put('https://api-content.dropbox.com/1/files_put/auto/'+path, data, {headers: authorizationHeader()});
+					return $http.put('https://api-content.dropbox.com/1/files_put/auto/'+path, data, {'headers': authorizationHeader()});
 				},
 				media: function(path){
-					return $http.post('https://api.dropbox.com/1/media/auto/'+path, null, {headers: authorizationHeader()});
+					return $http.post('https://api.dropbox.com/1/media/auto/'+path, null, {'headers': authorizationHeader()});
 				},
 				metadata: function(path){
 					if (!path || path == '') path = '/';
-					return $http.get('https://api.dropbox.com/1/metadata/auto/'+path, {params:{list: true, include_deleted:false}, headers: authorizationHeader()});
+					return $http.get('https://api.dropbox.com/1/metadata/auto/'+path, {'params':{'list': true,'include_deleted':false}, 'headers': authorizationHeader()});
 				},
 				previews: function(path, rev){
 					var params = {};
 					if (typeof(rev) != 'undefined') params.rev = rev;
-					return $http.get('ttps://api-content.dropbox.com/1/previews/auto/'+path, {params: params, headers: authorizationHeader()});
+					return $http.get('ttps://api-content.dropbox.com/1/previews/auto/'+path, {'params': params, 'headers': authorizationHeader()});
 				},
 				restore: function(path, rev){
-					return $http.post('https://api.dropbox.com/1/restore/auto/'+path, {rev: rev}, {headers: authorizationHeader()});
+					return $http.post('https://api.dropbox.com/1/restore/auto/'+path, {'rev': rev}, {'headers': authorizationHeader()});
 				},
 				revisions: function(path){
-					return $http.get('https://api.dropbox.com/1/revisions/auto/'+path, {params:{rev_limit:50}, headers: authorizationHeader()});
+					return $http.get('https://api.dropbox.com/1/revisions/auto/'+path, {'params':{'rev_limit':50}, 'headers': authorizationHeader()});
 				},
 				search: function(path, query){
-					return $http.post('https://api.dropbox.com/1/search/auto/'+path, {query: query, include_deleted: true}, {headers: authorizationHeader()});
+					return $http.post('https://api.dropbox.com/1/search/auto/'+path, {'query': query, 'include_deleted': false}, {'headers': authorizationHeader()});
 				},
 				
 				/* Helper and alias methods */
